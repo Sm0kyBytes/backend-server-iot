13b02d4cafadbe1e586989c51887be3cf1453884
Athikhom_ra <athikhom_ra@outlook.com>
2024-09-11 09:08:32 +0700

    feat(auth): add email in token payload

--- src/apps/auth.ts
+++ src/apps/auth.ts
@@ -120,6 +120,7 @@ authRouter.post(
           {
             id: userInfo.id,
             username: userInfo.username,
+            email: userInfo.email,
           },
           secretKey,
           {
